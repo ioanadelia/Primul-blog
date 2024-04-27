273cc674a55a353bd300eb112b2f72e9af062049
ioanadelia <168298144+ioanadelia@users.noreply.github.com>
2024-04-27 21:21:08 +0300

    Add files via upload

---
+++ HTML final/script.js
@@ -0,0 +1,11 @@
+function openArticles() {
+    window.location.href = 'index.html';
+}
+
+function openContact() {
+    window.location.href = 'Media.html';
+
+}
+function openReviews() {
+    window.location.href = 'Reviews.html';
+}
